9feed4c6717c6cc8fe8eeaf40d11046a36c2cfb6
aliyahhanun <liya.hanun02@gmail.com>
2023-06-25 23:31:27 +0700

    fcp web application

--- client/cookie.go
+++ client/cookie.go
@@ -18,6 +18,8 @@ func GetClientWithCookie(token string, cookies ...*http.Cookie) (*http.Client, e
 	})
 
 	jar.SetCookies(&url.URL{
+		// Scheme: "http",
+		// Host:	"localhost:8080",
 		Scheme: "https",
 		Host:   "fcp-web-application-production-68f4.up.railway.app",
 	}, cookies)
